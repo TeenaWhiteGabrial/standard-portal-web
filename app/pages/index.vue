496101709ddbd02826b27136664225800c214e5c
kunlun zhang <zhangkunlun01@inspur.com>
2025-09-02 15:00:00 +0800

    添加content展示

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,10 @@
 <template>
     <div w-full>首页</div>
+    <ContentRenderer :value="post || {}" />
 </template>
+<script setup lang="ts">
+    // const fooPost = await queryCollection('content').path('/articles/hello-world').first()
+    const { data: post } = await useAsyncData('/', () => {
+        return queryCollection('content').path('/articles/hello-world').first()
+    })
+</script>
