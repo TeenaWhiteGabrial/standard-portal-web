4cbbf99992cebc82a94c9755a88346012966bcc1
kunlun zhang <zhangkunlun01@inspur.com>
2025-09-03 15:51:27 +0800

    JSON可行性验证

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,35 @@
 <template>
     <div w-full>首页</div>
 
-    <ContentRenderer :value="post || {}" :data="post?.meta" />
+    <ContentRenderer :value="post" :data="post?.meta" />
 </template>
 <script setup lang="ts">
 
-    // const fooPost = await queryCollection('content').path('/articles/hello-world').first()
-    const { data: post } = await useAsyncData('/', () => {
-        return queryCollection('content').path('/carousel/information').first()
-    })
+    const post = {
+        "id": "content/carousel/information.md",
+        "title": "Demo演示",
+        "body": {
+            "type": "minimark",
+            "value": [
+                [
+                    "carousel-policy",
+                    {
+                        ":title": "author",
+                        ":headertitle": "header.description"
+                    }
+                ]
+            ],
+        },
+        "meta": {
+            "author": "孙悟空",
+            "date": "2025-09-03",
+            "headerTitle": "标题党",
+            "header": {
+                "content": "标题党1",
+                "description": "旨在验证markdown是否可行"
+            },
+            "summary": "旨在验证markdown是否可行",
+            "cover": "/images/cover.jpg"
+        },
+    }
 </script>
